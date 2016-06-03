41a0dfd8fae695e64d717692577d0b2e9bbe13cd
이상규 <lp31004@gmail.com>
2016-06-03 19:02:19 +0900

    Modified login.html db.php

--- src/db_io.php
+++ src/db_io.php
@@ -5,6 +5,19 @@ $db = new PDO("mysql:dbname=babycare;host=172.16.24.89","root","master123");
 function insert($table, $data) {
 	switch($table) {
 	case "USER":
+		$user_num = $db->quote(hash($data["id"]));
+		$id = $db->quote($data["id"]);
+		$password = $db->quote($data["password"]);
+		$name = $db->quote($data["name"]);
+		$age = $db->quote(date("Y") - $data["year"] + 1);
+		$baby = $db->quote(date("Y") - $data["baby_year"] + 1);
+		$phone = $db->quote($data["phone"]."-".$data["middle_number"]."-".$data["last_number"]);
+		$location = $db->quote($data["location"]);
+		$is_single = $db->quote($data["is_single"]);
+		$gender = $db->quote($data["gender"]);
+
+		$query = "insert into USER values($user_num, $id, $password, $name, $age, $baby, $phone, $location, $is_single, $gender)";
+		$db->exec($query);
 		break;
 
 	case "POST":
