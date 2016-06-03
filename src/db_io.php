b90e72f7d9543a5cd7673ec4d46d68b409353a5f
unknown <>
2016-06-04 00:33:05 +0900

    now login is working.

--- src/db_io.php
+++ src/db_io.php
@@ -1,24 +1,24 @@
 <?
 /* DB_IO.php */
-$db = new PDO("mysql:dbname=babycare;host=172.16.24.89","root","master123");
+$db = new PDO("mysql:dbname=babycare;host=localhost","root","apmsetup");
 
 function insert($table, $data) {
 	global $db;
 
 	switch($table) {
 	case "USER":
-		$user_num = $db->quote(hash($data["id"]));
+		$user_num = $db->quote(hash("md5", $data["id"]));
 		$id = $db->quote($data["id"]);
 		$password = $db->quote($data["password"]);
 		$name = $db->quote($data["name"]);
-		$age = $db->quote(date("Y") - $data["year"] + 1);
-		$baby = $db->quote(date("Y") - $data["baby_year"] + 1);
+		$age = date(Y) - $data["year"] + 1;
+		$baby = date(Y) - $data["baby_year"] + 1;
 		$phone = $db->quote($data["phone"]."-".$data["middle_number"]."-".$data["last_number"]);
 		$location = $db->quote($data["location"]);
 		$is_single = $db->quote($data["is_single"]);
 		$gender = $db->quote($data["gender"]);
 
-		$query = "insert into USER values($user_num, $id, $password, $name, $age, $baby, $phone, $location, $is_single, $gender)";
+		$query = "insert into USER values($user_num, $id, $password, $name, $age, $baby, $phone, $location, 'y', 'm')";
 		$db->exec($query);
 		break;
 
@@ -42,9 +42,9 @@ function is_exist($table, $id) {
 	switch($table) {
 	case "USER":
 		$id = $db->quote($id);
-		$query = "select * from user where id=$id";
+		$query = "select count(*) from user where id=$id";
 
-		$result = $db->exec($query);
+		$result = $db->query($query);
 		break;
 
 	case "POST":
@@ -60,9 +60,26 @@ function is_exist($table, $id) {
 		break;
 	}
 
-	if($result)
+	if($result->fetchColumn() > 0)
 		return TRUE;
 	else
 		return FALSE;
 }
+
+function valid($id,$password) {
+	global $db;
+	$id = $db->quote($id);
+	$password = $db->quote($password);
+	$query = "select count(*) from user where id=$id and password=$password";
+
+	$result = $db->query($query);
+
+	if($result->fetchColumn() == 1) {
+		return TRUE;
+	} else {
+		return FALSE;
+		
+	}
+}
+
 ?>
